572dc16306ed517b30609d5b46e846ff2dd30807
longarms07 <ecridd@gmail.com>
2020-07-21 14:50:17 -0600

    Attempted to make thumb pointing get detected correct

--- src/gestures/Fist.js
+++ src/gestures/Fist.js
@@ -3,6 +3,9 @@ import GestureDescription from '../GestureDescription';
 
 const gestureDescription = new GestureDescription('fist');
 
+gestureDescription.addCurl(Finger.Thumb, FingerCurl.NoCurl, -0.5);
+gestureDescription.addCurl(Finger.Index, FingerCurl.NoCurl, -0.5);
+
 for (let finger of [Finger.Thumb, Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
   gestureDescription.addCurl(finger, FingerCurl.FullCurl, 2.0);
   gestureDescription.addCurl(finger, FingerCurl.HalfCurl, 0.5);
